033db544e4e5ba11a3ebebc9ba02cd83091c6141
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-06-22 15:48:19 +0700

    feat: added Stall CRUD Feature

---
+++ app/Models/Stall.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stall extends Model
+{
+    use CrudTrait;
+    use HasFactory;
+
+    protected $fillable = [
+        'long',
+        'lat',
+        'status',
+        'areaId',
+    ];
+}
