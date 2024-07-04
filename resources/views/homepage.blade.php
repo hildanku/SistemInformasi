e710f596917cf05fdd7ea64a03c99bb33c3a9fd1
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-07-05 00:07:49 +0700

    feat: added homepage(for visitor)

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,14 @@
+@extends('_voler.master')
+
+@section('content')
+<section class="section">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Dashboard</h5>
+                <p class="card-text">Selamat Datang, silahkan registrasi terlebih dahulu untuk melanjutkan</p>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
