ed1efc4b83a5dd8db84283e140cbaffd86dd57d6
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-07-05 00:36:26 +0700

    chore(seeders/locationseeder): implement LocationSeeder for initial location entries

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -14,6 +14,29 @@ class LocationSeeder extends Seeder
      */
     public function run(): void
     {
-
+        $coordinates = [
+            [109.651284, -7.669550],
+            [109.650978, -7.669111],
+            [109.651028, -7.668332],
+            [109.651436, -7.667990],
+            [109.652313, -7.667945],
+            [109.652619, -7.668279],
+            [109.652631, -7.668887],
+            [109.652264, -7.669489],
+        ];
+        
+        $locations = [];
+        foreach ($coordinates as $index => $coord) {
+            $locations[] = [
+                'locationCode' => 'LOC' . ($index + 1),
+                'long' => (string)$coord[0],
+                'lat' => (string)$coord[1],
+                'status' => $index % 2 == 0 ? 'available' : 'unavailable',
+                'areaId' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+        }
+        DB::table('locations')->insert($locations);
     }
 }
